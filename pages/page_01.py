c42b0d34480e7f935ffeb80d8e75804ab67d1b0d
Vlad Drozhzhin <Faskerrr@gmail.com>
2025-12-05 13:19:04 +0400

    start of my frontend repo

---
+++ pages/page_01.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+
+st.markdown('# Welcome to my 1st page')
---
+++ pages/page_02.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+st.markdown('# Welcome to my 2nd page'
+            '## Almost as cool as the 1st one')
